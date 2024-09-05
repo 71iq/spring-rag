858c705322aa9d3b0daa34f68407d46d48440148
Ehab Maali <ehab1.maali@gmail.com>
2024-09-05 06:57:55 +0300

    with history

--- src/main/java/ollama/ehab/rag/service/ChatService.java
+++ src/main/java/ollama/ehab/rag/service/ChatService.java
@@ -8,6 +8,7 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.UUID;
@@ -31,7 +32,7 @@ public class ChatService {
     }
 
     public ChatResponse chat(String chatId, String message) {
-        Message systemMessage = promptManagementService.getSystemMessage(message);
+        Message systemMessage = promptManagementService.getSystemMessage(chatId, message);
         UserMessage userMessage = new UserMessage(message);
         promptManagementService.addMessage(chatId, userMessage);
         logger.debug("Chatting with chatId: {} and message: {}", chatId, message);
